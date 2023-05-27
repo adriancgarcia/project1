314e20f5b77560fa90211f7ec1ac84bcb99e3fa6
adriancgarcia <adriancgarcia@gmail.com>
2023-05-27 16:31:11 -0500

    added more styling, input, and search buttons

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-console.log("hello hello!")
+// console.log("hello hello!")
 
-$.ajax("https://swapi.dev/api/")
-.then((data) => {
-    console.log(data)
-    console.log(data.starships)
-})
+const addSquare = (results) => {
+    const square = document.createElement("div")
+    square.classList.add("square")
+    square.innerHTML = `<h2>${results}</h2>`
+    const container = document.querySelector(".results-container")
+    container.append(square)
+}
 
-$.ajax("https://swapi.dev/api/starships/")
-.then((data1) => {
-    console.log(data1)
-    console.log(data1.results)
-})
+
+// $.ajax("https://swapi.dev/api/")
+// .then((data) => {
+//     console.log(data) 
+//     console.log(data.results)
+//     addSquare(data.results)
+// })
+
+
+const showResultMakeSquare = () => {
+
+    $.ajax("https://swapi.dev/api/people/?search")
+    .then((data1) => {
+        console.log(data1)
+        console.log(data1)
+        addSquare(data1)
+    }) 
+} 
+
+// showResultMakeSquare ()
+
+// const input = document.querySelector("input")
+// input.addEventListener("click", showResultMakeSquare)
